ef19dca90b15e0caddac24efb801382ef0fdc11b
exosty <jarisob@gmail.com>
2016-06-25 13:30:30 +0300

    update ABCrules

--- codility/ABCrules.rb
+++ codility/ABCrules.rb
@@ -1,4 +1,6 @@
 def solution(s)
     s.scan(/[AB]*(A)[AB]*|[BC]*(C)[BC]*|(B+)/).join('')
 end
+#any part of string containing only A and B symbols transforms into A
+#any part of string containing only C and B symbols transforms into C
 puts solution('ACBABACBACBACBACBCBACACBACBAABBAAAABABACC')
